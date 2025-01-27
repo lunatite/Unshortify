e8674e199fd0ff72141f0e1a921d27a18acfd939
lunatite <bedgowns@gmail.com>
2025-01-27 00:15:33 -0800

    chore: removed unused services from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,9 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
-import { Sub2GetService } from "./link-shortener/services/sub2get.service";
-import { BoostInkService } from "./link-shortener/services/boostink.service";
-import { AdFocusService } from "./link-shortener/services/adfocus.service";
-import { MBoostMeService } from "./link-shortener/services/mboostme.service";
-import { LootLabsService } from "./link-shortener/services/lootlabs.service";
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [
-    Sub2GetService,
-    AdFocusService,
-    BoostInkService,
-    MBoostMeService,
-    LootLabsService,
-    AppService,
-  ],
+  providers: [],
 })
 export class AppModule {}
