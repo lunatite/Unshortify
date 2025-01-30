c209512a736ad22ea9ad2ff353f738ae0f02b6ba
lunatite <bedgowns@gmail.com>
2025-01-30 15:04:02 -0800

    chore: configure static file serving with ServeStaticModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from "@nestjs/common";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
 import { BypassModule } from "./bypass/bypass.module";
 
 @Module({
-  imports: [BypassModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "..", "client"),
+    }),
+    BypassModule,
+  ],
   controllers: [],
   providers: [],
 })
