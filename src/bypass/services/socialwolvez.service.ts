1247af75606ce82c3269d8a22e5878e07a17214d
lunatite <bedgowns@gmail.com>
2025-02-01 01:56:39 -0800

    feat: add validation for path segments in URL

--- src/bypass/services/socialwolvez.service.ts
+++ src/bypass/services/socialwolvez.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from "@nestjs/common";
 import { LinkProcessorHandler } from "../link-processor.types";
+import { InvalidPathException } from "src/common/errors/invalid-path.exception";
 
 @Injectable()
 export class SocialWolvezService implements LinkProcessorHandler {
   public readonly name = "SocialWolvez";
+  private readonly requiredPathSegments = 4;
 
   async resolve(url: URL) {
+    if (
+      url.pathname === "/" ||
+      url.pathname.split("/").length < this.requiredPathSegments
+    ) {
+      throw new InvalidPathException("/app/l/{id}");
+    }
+
     return "";
   }
 }
