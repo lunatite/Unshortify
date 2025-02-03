d589ff314bb6ac5b7924636c9fadb47462dd97e9
lunatite <bedgowns@gmail.com>
2025-02-02 17:34:52 -0800

    feat: add caching to rekonise service

--- src/bypass/services/rekonise.service.ts
+++ src/bypass/services/rekonise.service.ts
@@ -1,11 +1,28 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Inject } from "@nestjs/common";
 import axios from "axios";
 import { LinkProcessorHandler } from "../link-processor.types";
 import { InvalidPathException } from "src/common/errors/invalid-path.exception";
+import { CacheService } from "./shared/cache/cache.service";
+import { Cache, CACHE_MANAGER } from "@nestjs/cache-manager";
 
 @Injectable()
-export class RekoniseService implements LinkProcessorHandler {
+export class RekoniseService
+  extends CacheService
+  implements LinkProcessorHandler
+{
   public readonly name = "Rekonise";
+
+  constructor(@Inject(CACHE_MANAGER) cache: Cache) {
+    super(cache);
+  }
+
+  private async fetchBypassedLink(id: string) {
+    const { data } = await axios.get<{ url: string; date: string }>(
+      `https://api.rekonise.com/social-unlocks/${id}`,
+    );
+
+    return data.url;
+  }
 
   async resolve(url: URL) {
     if (url.pathname === "/") {
@@ -13,11 +30,15 @@ export class RekoniseService implements LinkProcessorHandler {
     }
 
     const id = url.pathname.slice(1, url.pathname.length);
+    const cachedBypassedLink = await this.getFromCache<string>(id);
 
-    const { data } = await axios.get<{ url: string; date: string }>(
-      `https://api.rekonise.com/social-unlocks/${id}`,
-    );
+    if (cachedBypassedLink) {
+      return cachedBypassedLink;
+    }
 
-    return data.url;
+    const bypassedLink = await this.fetchBypassedLink(id);
+
+    await this.storeInCache(id, bypassedLink, 1000 * 60 * 60 * 24);
+    return bypassedLink;
   }
 }
