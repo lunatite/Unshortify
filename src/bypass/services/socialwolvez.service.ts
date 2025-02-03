f94c82a8d8a2f0c8ebd4c42cdb22f03a287da391
lunatite <bedgowns@gmail.com>
2025-02-02 17:38:53 -0800

    feat: add caching to socialwolvez service

--- src/bypass/services/socialwolvez.service.ts
+++ src/bypass/services/socialwolvez.service.ts
@@ -1,27 +1,30 @@
 import {
   BadRequestException,
+  Inject,
   Injectable,
   InternalServerErrorException,
 } from "@nestjs/common";
 import axios from "axios";
 import * as cheerio from "cheerio";
+import { CACHE_MANAGER, Cache } from "@nestjs/cache-manager";
 import { LinkProcessorHandler } from "../link-processor.types";
 import { InvalidPathException } from "src/common/errors/invalid-path.exception";
+import { CacheService } from "./shared/cache/cache.service";
 
 @Injectable()
-export class SocialWolvezService implements LinkProcessorHandler {
+export class SocialWolvezService
+  extends CacheService
+  implements LinkProcessorHandler
+{
   public readonly name = "SocialWolvez";
   private readonly requiredPathSegments = 4;
   private readonly targetNuxtDataIndex = 5;
 
-  async resolve(url: URL) {
-    if (
-      url.pathname === "/" ||
-      url.pathname.split("/").length < this.requiredPathSegments
-    ) {
-      throw new InvalidPathException("/app/l/{id}");
-    }
+  constructor(@Inject(CACHE_MANAGER) cache: Cache) {
+    super(cache);
+  }
 
+  private async fetchBypassLink(url: URL) {
     const { data: htmlContent } = await axios.get(url.href);
     const $ = cheerio.load(htmlContent);
 
@@ -41,5 +44,26 @@ export class SocialWolvezService implements LinkProcessorHandler {
     }
 
     return jsonNuxtData[this.targetNuxtDataIndex];
+  }
+
+  async resolve(url: URL) {
+    if (
+      url.pathname === "/" ||
+      url.pathname.split("/").length < this.requiredPathSegments
+    ) {
+      throw new InvalidPathException("/app/l/{id}");
+    }
+
+    const id = url.pathname.split("/")[this.requiredPathSegments - 1];
+    const cachedBypassedLink = await this.getFromCache<string>(id);
+
+    if (cachedBypassedLink) {
+      return cachedBypassedLink;
+    }
+
+    const bypassedLink = await this.fetchBypassLink(url);
+    await this.storeInCache(id, bypassedLink, 1000 * 60 * 60 * 24);
+
+    return bypassedLink;
   }
 }
