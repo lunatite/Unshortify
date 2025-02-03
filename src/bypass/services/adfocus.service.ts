13ce0f555a38e285202d19aec8e7da03098a5bc3
lunatite <bedgowns@gmail.com>
2025-02-02 16:05:58 -0800

    feat: add caching to adfocus service

--- src/bypass/services/adfocus.service.ts
+++ src/bypass/services/adfocus.service.ts
@@ -1,17 +1,24 @@
+import { Inject } from "@nestjs/common";
+import { Cache, CACHE_MANAGER } from "@nestjs/cache-manager";
 import axios from "axios";
+import { CacheService } from "./shared/cache/cache.service";
 import { LinkProcessorHandler } from "../link-processor.types";
 import { InvalidPathException } from "src/common/errors/invalid-path.exception";
 import { BypassLinkNotFoundException } from "../exceptions/bypass-link-not-found.exception";
 
-export class AdFocusService implements LinkProcessorHandler {
-  public readonly name = "Adfoc.us";
+export class AdFocusService
+  extends CacheService
+  implements LinkProcessorHandler
+{
+  public readonly name = "Adfocus";
   private readonly clickUrlRegex = /var click_url\s*=\s*"([^"]+)"/;
+  private readonly twoHoursToMilliseconds = 2 * 60 * 60 * 1000;
 
-  async resolve(url: URL): Promise<string> {
-    if (url.pathname === "/") {
-      throw new InvalidPathException("/${id}");
-    }
+  constructor(@Inject(CACHE_MANAGER) cache: Cache) {
+    super(cache);
+  }
 
+  private async fetchBypassedLink(url: URL) {
     const { data: htmlContent } = await axios.get(url.href, {
       responseType: "text",
       headers: {
@@ -28,5 +35,23 @@ export class AdFocusService implements LinkProcessorHandler {
 
     const bypassedUrl = bypassedUrlMatch[1];
     return bypassedUrl;
+  }
+
+  async resolve(url: URL): Promise<string> {
+    if (url.pathname === "/") {
+      throw new InvalidPathException("/${id}");
+    }
+
+    const id = url.pathname.split("/")[1];
+    const cachedBypassedLink = await this.getFromCache<string>(id);
+
+    if (cachedBypassedLink) {
+      return cachedBypassedLink;
+    }
+
+    const bypassedLink = await this.fetchBypassedLink(url);
+    await this.storeInCache(id, bypassedLink, this.twoHoursToMilliseconds);
+
+    return bypassedLink;
   }
 }
