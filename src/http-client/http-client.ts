3ec10a6f489e44525746e4a4e4fe75f9bd99fe18
lunatite <bedgowns@gmail.com>
2025-02-03 16:34:31 -0800

    feat: make proxy optional in HttpClient class

--- src/http-client/http-client.ts
+++ src/http-client/http-client.ts
@@ -10,10 +10,18 @@ import { HttpsProxyAgent } from "https-proxy-agent";
 export class HttpClient {
   private readonly axiosInstance: AxiosInstance;
 
-  constructor(proxy: string) {
-    const proxyAgent = new HttpsProxyAgent(proxy, {
-      ciphers: "TLS_AES_128_GCM_SHA256",
-    });
+  constructor(proxy?: string) {
+    let httpsAgent: https.Agent;
+
+    if (proxy) {
+      httpsAgent = new HttpsProxyAgent(proxy, {
+        ciphers: "TLS_AES_128_GCM_SHA256",
+      });
+    } else {
+      httpsAgent = new https.Agent({
+        ciphers: "TLS_AES_128_GCM_SHA256",
+      });
+    }
 
     this.axiosInstance = axios.create({
       timeout: 5000,
@@ -22,7 +30,7 @@ export class HttpClient {
           "Mozilla/5.0 (X11; Linux x86_64; rv:135.0) Gecko/20100101 Firefox/135.0",
       },
       // Cloudflare can detect that Node.js is making the request, so simply change the cipher.
-      httpsAgent: proxyAgent,
+      httpsAgent,
     });
   }
 
--- src/utils/isValidProxy.ts
+++ src/utils/isValidProxy.ts
@@ -1,4 +1,4 @@
-const proxyRegex = /^(https?:\/\/)([a-zA-Z0-9.-]+)(:\d+)?$/;
+const proxyRegex = /^(https?:\/\/)?([^@:]+):([^@]+)@([a-zA-Z0-9.-]+):(\d+)$/;
 
 export function isValidProxy(line: string): boolean {
   return proxyRegex.test(line);
