e7a08ca66cf2a215d6e5c53e71d6aedb477759f6
lunatite <bedgowns@gmail.com>
2025-02-15 02:43:24 -0800

    feat: extract target URLs from shortened link page

--- src/bypass/services/linkunlocker.service.ts
+++ src/bypass/services/linkunlocker.service.ts
@@ -1,18 +1,62 @@
 import { HttpService } from "@nestjs/axios";
-import { Injectable } from "@nestjs/common";
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from "@nestjs/common";
 import { LinkProcessorHandler } from "../link-processor.types";
 import { InvalidPathException } from "src/common/errors/invalid-path.exception";
+
+type TargetUrlsResponse = Array<{
+  actionId: string;
+  targetUrl: string;
+  _id: string;
+}>;
 
 @Injectable()
 export class LinkUnlockerService implements LinkProcessorHandler {
   public readonly name = "LinkUnlocker";
+  private readonly targetUrlsRegex = /\\"targetUrls\\":(.*?),\\"coverImage\\"/;
 
   constructor(private readonly httpService: HttpService) {}
 
-  async resolve(url: URL) {
-    if (url.pathname === "/") {
-      throw new InvalidPathException("/{id}");
+  private createAnonymousId() {
+    return crypto.randomUUID();
+  }
+
+  private async getTargetUrls(url: URL): Promise<TargetUrlsResponse> {
+    const { data: html } = await this.httpService.axiosRef.get(url.href, {
+      responseType: "text",
+    });
+
+    const targetUrlsMatch = this.targetUrlsRegex.exec(html);
+
+    if (!targetUrlsMatch || !targetUrlsMatch[1]) {
+      throw new BadRequestException(
+        "Could not find target URLs in the HTML response",
+      );
     }
+
+    const jsonString = targetUrlsMatch[1].replace(/\\"/g, '"');
+
+    try {
+      const parsedData = JSON.parse(jsonString);
+
+      if (!Array.isArray(parsedData) || parsedData.length === 0) {
+        throw new Error("Extracted data is not a valid array");
+      }
+
+      return parsedData;
+    } catch {
+      throw new BadRequestException(
+        "Failed to parse JSON from extracted target URLs",
+      );
+    }
+  }
+
+  async resolve(url: URL) {
+    const targetUrls = await this.getTargetUrls(url);
+    console.log(targetUrls);
 
     return "";
   }
