fcd108a9044b4449beee6635be8e637e926eb137
lunatite <bedgowns@gmail.com>
2025-02-15 03:05:04 -0800

    feat: update target URL extraction to scrape page metadata

--- src/bypass/services/linkunlocker.service.ts
+++ src/bypass/services/linkunlocker.service.ts
@@ -7,16 +7,17 @@ import {
 import { LinkProcessorHandler } from "../link-processor.types";
 import { InvalidPathException } from "src/common/errors/invalid-path.exception";
 
-type TargetUrlsResponse = Array<{
+type UnlockAction = {
   actionId: string;
   targetUrl: string;
   _id: string;
-}>;
+};
 
 @Injectable()
 export class LinkUnlockerService implements LinkProcessorHandler {
   public readonly name = "LinkUnlocker";
   private readonly targetUrlsRegex = /\\"targetUrls\\":(.*?),\\"coverImage\\"/;
+  private readonly secureTargetRegex = /\"_secureTarget\\":\\"(.*?)"/;
 
   constructor(private readonly httpService: HttpService) {}
 
@@ -55,7 +56,13 @@ export class LinkUnlockerService implements LinkProcessorHandler {
     return response.data.token;
   }
 
-  private async getTargetUrls(url: URL): Promise<TargetUrlsResponse> {
+  private async decryptUrl(
+    encryptedUrl: string,
+    requestToken: string,
+    unlockerId: string,
+  ) {}
+
+  private async getPageMetadata(url: URL) {
     const { data: html } = await this.httpService.axiosRef.get(url.href, {
       responseType: "text",
     });
@@ -68,16 +75,28 @@ export class LinkUnlockerService implements LinkProcessorHandler {
       );
     }
 
+    const secureTargetMatch = this.secureTargetRegex.exec(html);
+
+    if (!secureTargetMatch || !secureTargetMatch[1]) {
+      throw new BadRequestException(
+        "Could not find _secureTarget in the HTML response",
+      );
+    }
+
+    const secureTarget = secureTargetMatch[1];
     const jsonString = targetUrlsMatch[1].replace(/\\"/g, '"');
 
     try {
-      const parsedData = JSON.parse(jsonString);
+      const parsedData = JSON.parse(jsonString) as Array<UnlockAction>;
 
       if (!Array.isArray(parsedData) || parsedData.length === 0) {
         throw new Error("Extracted data is not a valid array");
       }
 
-      return parsedData;
+      return {
+        secureTarget,
+        targetUrls: parsedData,
+      };
     } catch {
       throw new BadRequestException(
         "Failed to parse JSON from extracted target URLs",
@@ -87,10 +106,12 @@ export class LinkUnlockerService implements LinkProcessorHandler {
 
   async resolve(url: URL) {
     const anonymousId = this.createAnonymousId();
-    const targetUrls = await this.getTargetUrls(url);
+    const { secureTarget, targetUrls } = await this.getPageMetadata(url);
+
+    console.log(secureTarget, targetUrls);
 
     // await this.sendAnalytic("pageView", targetUrls[0]._id, anonymousId);
-    await this.generateUnlockerToken(targetUrls[0]._id);
+    // await this.generateUnlockerToken(targetUrls[0]._id);
 
     return "";
   }
