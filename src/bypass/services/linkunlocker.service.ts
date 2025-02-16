9f3926641d49f5d759bf5940df3665205afd6d80
lunatite <bedgowns@gmail.com>
2025-02-15 20:55:40 -0800

    feat: add bypass functionality for linkunlocker

--- src/bypass/services/linkunlocker.service.ts
+++ src/bypass/services/linkunlocker.service.ts
@@ -1,46 +1,18 @@
 import { HttpService } from "@nestjs/axios";
-import {
-  BadRequestException,
-  Injectable,
-  InternalServerErrorException,
-} from "@nestjs/common";
+import * as cheerio from "cheerio";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { LinkProcessorHandler } from "../link-processor.types";
 import { InvalidPathException } from "src/common/errors/invalid-path.exception";
-
-type UnlockAction = {
-  actionId: string;
-  targetUrl: string;
-  _id: string;
-};
+import { extractMatch } from "src/utils/extractMatch";
 
 @Injectable()
 export class LinkUnlockerService implements LinkProcessorHandler {
   public readonly name = "LinkUnlocker";
-  private readonly targetUrlsRegex = /\\"targetUrls\\":(.*?),\\"coverImage\\"/;
-  private readonly secureTargetRegex = /\"_secureTarget\\":\\"(.*?)"/;
+  private readonly unlockerIdRegex = /\\"_id\\":\\"(.*?)\\"/;
+  private readonly secureTargetRegex = /\\"_secureTarget\\":\\"(.*?)(\\|)\"/;
+  private readonly randomIdRegex = /self\.__next_f\.push\(\[\d*,\"(.*?)\\"/;
 
   constructor(private readonly httpService: HttpService) {}
-
-  private createAnonymousId() {
-    return crypto.randomUUID();
-  }
-
-  //   private async sendAnalytic(
-  //     type: "pageView",
-  //     unlockerId: string,
-  //     anonymousId: string,
-  //   ) {
-  //     await this.httpService.axiosRef.post<RecordAnalyticResponse>(
-  //       "https://linkunlocker.com/api/analytics",
-  //       {
-  //         anonymousId,
-  //         creatorId: null,
-  //         type,
-  //         unlockerId,
-  //         userId: null,
-  //       },
-  //     );
-  //   }
 
   private async generateUnlockerToken(unlockerId: string) {
     const response = await this.httpService.axiosRef.post<{ token: string }>(
@@ -58,61 +30,76 @@ export class LinkUnlockerService implements LinkProcessorHandler {
 
   private async decryptUrl(
     encryptedUrl: string,
-    requestToken: string,
+    unlockerToken: string,
     unlockerId: string,
-  ) {}
+  ) {
+    const response = await this.httpService.axiosRef.post<{ url: string }>(
+      "https://linkunlocker.com/api/decrypt-url",
+      {
+        encryptedUrl,
+        requestToken: unlockerToken,
+        unlockerId,
+      },
+      {
+        headers: {
+          Origin: "https://linkunlocker.com",
+        },
+      },
+    );
 
-  private async getPageMetadata(url: URL) {
+    return response.data.url;
+  }
+
+  private extractRandomIdFromScript(html: string) {
+    const $ = cheerio.load(html);
+    const scripts = $("script");
+    const scriptContent = scripts.eq(scripts.length - 5).html();
+
+    if (!scriptContent) {
+      throw new Error("Could not extract script content");
+    }
+
+    return extractMatch(scriptContent, this.randomIdRegex, "random id");
+  }
+
+  private async getPageMetadata(
+    url: URL,
+  ): Promise<{ encryptedId: string; unlockerId: string }> {
     const { data: html } = await this.httpService.axiosRef.get(url.href, {
       responseType: "text",
     });
 
-    const targetUrlsMatch = this.targetUrlsRegex.exec(html);
+    const unlockerId = extractMatch(html, this.unlockerIdRegex, "unlocker id");
+    const secureTarget = extractMatch(
+      html,
+      this.secureTargetRegex,
+      "secure target",
+    );
 
-    if (!targetUrlsMatch || !targetUrlsMatch[1]) {
-      throw new BadRequestException(
-        "Could not find target URLs in the HTML response",
-      );
-    }
-
-    const secureTargetMatch = this.secureTargetRegex.exec(html);
-
-    if (!secureTargetMatch || !secureTargetMatch[1]) {
-      throw new BadRequestException(
-        "Could not find _secureTarget in the HTML response",
-      );
-    }
-
-    const secureTarget = secureTargetMatch[1];
-    const jsonString = targetUrlsMatch[1].replace(/\\"/g, '"');
-
-    try {
-      const parsedData = JSON.parse(jsonString) as Array<UnlockAction>;
-
-      if (!Array.isArray(parsedData) || parsedData.length === 0) {
-        throw new Error("Extracted data is not a valid array");
-      }
-
+    if (secureTarget.length > 60) {
       return {
-        secureTarget,
-        targetUrls: parsedData,
+        encryptedId: secureTarget,
+        unlockerId,
       };
-    } catch {
-      throw new BadRequestException(
-        "Failed to parse JSON from extracted target URLs",
-      );
     }
+
+    const randomId = this.extractRandomIdFromScript(html);
+
+    return {
+      encryptedId: secureTarget + randomId,
+      unlockerId,
+    };
   }
 
   async resolve(url: URL) {
-    const anonymousId = this.createAnonymousId();
-    const { secureTarget, targetUrls } = await this.getPageMetadata(url);
+    const { encryptedId, unlockerId } = await this.getPageMetadata(url);
+    const unlockerToken = await this.generateUnlockerToken(unlockerId);
+    const decryptedUrl = await this.decryptUrl(
+      encryptedId,
+      unlockerToken,
+      unlockerId,
+    );
 
-    console.log(secureTarget, targetUrls);
-
-    // await this.sendAnalytic("pageView", targetUrls[0]._id, anonymousId);
-    // await this.generateUnlockerToken(targetUrls[0]._id);
-
-    return "";
+    return decryptedUrl;
   }
 }
