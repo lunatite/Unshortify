6c3c24afca408049842cca5085f5ef255546bd6e
lunatite <bedgowns@gmail.com>
2025-02-21 01:53:26 -0800

    feat: add responseURl existence check to AdFocusService for better error handling

--- src/bypass/services/adfocus.service.ts
+++ src/bypass/services/adfocus.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { HttpService } from "@nestjs/axios";
 import { LinkProcessorHandler } from "../link-processor.types";
 import { InvalidPathException } from "src/common/errors/invalid-path.exception";
@@ -12,18 +12,19 @@ export class AdFocusService implements LinkProcessorHandler {
   constructor(private readonly httpService: HttpService) {}
 
   private async fetchBypassedLink(url: URL) {
-    const { data: htmlContent } = await this.httpService.axiosRef.get<string>(
-      url.href,
-      {
-        responseType: "text",
-        headers: {
-          Accept:
-            "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        },
+    const response = await this.httpService.axiosRef.get<string>(url.href, {
+      responseType: "text",
+      headers: {
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
       },
-    );
+    });
 
-    const bypassedUrlMatch = this.clickUrlRegex.exec(htmlContent);
+    if (response.request.res?.responseUrl === "https://adfoc.us/") {
+      throw new BadRequestException("The requested resource cannot be found");
+    }
+
+    const bypassedUrlMatch = this.clickUrlRegex.exec(response.data);
 
     if (!bypassedUrlMatch || !bypassedUrlMatch[1]) {
       throw new BypassLinkNotFoundException();
