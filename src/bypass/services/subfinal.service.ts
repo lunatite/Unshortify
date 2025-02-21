82f65d3387ed6326b50fba307376875a308fbe44
lunatite <bedgowns@gmail.com>
2025-02-20 20:54:45 -0800

    feat: add additional check for SubFinal URL existence before processing

--- src/bypass/services/subfinal.service.ts
+++ src/bypass/services/subfinal.service.ts
@@ -1,14 +1,14 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { HttpService } from "@nestjs/axios";
 import { LinkProcessorHandler } from "../link-processor.types";
 import { InvalidPathException } from "src/common/errors/invalid-path.exception";
 import { BypassLinkNotFoundException } from "../exceptions/bypass-link-not-found.exception";
-import { HttpService } from "@nestjs/axios";
 
 @Injectable()
 export class SubFinalService implements LinkProcessorHandler {
   public readonly name = "SubFinal";
 
-  private readonly fileRegex = /window\.open\("(.*)","_self"\);/;
+  private readonly windowOpenUrlRegex = /window\.open\("(.*)","_self"\);/;
 
   constructor(private readonly httpService: HttpService) {}
 
@@ -17,13 +17,19 @@ export class SubFinalService implements LinkProcessorHandler {
       `https://subfinal.com/final.php?$=${id}&own=owner`,
     );
 
-    const fileRegexMatch = this.fileRegex.exec(htmlContent);
+    const fileRegexMatch = this.windowOpenUrlRegex.exec(htmlContent);
 
     if (!fileRegexMatch || fileRegexMatch[1] === undefined) {
       throw new BypassLinkNotFoundException();
     }
 
     const bypassedLink = fileRegexMatch[1];
+
+    // if the bypassed link is empty it means that it doesn't exist.
+    if (bypassedLink === "") {
+      throw new BadRequestException("The requested resource cannot be found");
+    }
+
     return bypassedLink;
   }
 
