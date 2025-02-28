48ef883f4b774f1d59f4354e18dad4e0512cf72f
lunatite <bedgowns@gmail.com>
2025-02-28 00:05:46 -0800

    feat: add URL validation to ensure correct format

--- src/unlocker/services/link-bypass/cutyion.ts
+++ src/unlocker/services/link-bypass/cutyion.ts
@@ -2,19 +2,29 @@ import { HttpService } from "@nestjs/axios";
 import { Injectable } from "@nestjs/common";
 import { SupportedHosts } from "src/unlocker/decorators/supported-hosts.decorator";
 import { UnlockerResult, UnlockerService } from "../unlocker.type";
+import { InvalidPathException } from "src/common/errors/invalid-path.exception";
 
 @Injectable()
 @SupportedHosts(["cutyion.com"])
 export class CutyionService implements UnlockerService {
   public readonly name = "Cutyion";
+  public static readonly BASE_URL = "https://cutyion.com/";
 
   async unlock(
     url: URL,
     metadata?: Record<string, unknown>,
   ): Promise<UnlockerResult> {
+    const parts = url.pathname.split("/").filter(Boolean);
+
+    if (parts.length !== 1) {
+      throw new InvalidPathException("/{id}");
+    }
+
+    const id = parts[0];
+
     return {
       type: "url",
-      content: "",
+      content: id,
     };
   }
 }
